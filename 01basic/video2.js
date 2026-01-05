e142d96320bda0203f4dcf908e6bbd69f2e8f17f
Ayan Saha <ayanwork2003@gmail.com>
2026-01-05 12:42:36 +0000

    let , var , const , console

---
+++ 01basic/video2.js
@@ -0,0 +1,18 @@
+const accountName="Permanant";
+
+let accountId = " Idef9";
+
+var accountType="domestic";
+
+accountCity="bangalore";
+    let accountDist;
+
+// we have to reduce the usgae of var it is of no use we can use const and let //
+//const && let//
+
+//to print the whole things all together we can use consple.table instead of console.log//
+// it can create the table of the output//
+
+// if we do not declare any type the value in the memeory remains undefined//
+console.table([accountId,accountName, accountType , accountCity , accountDist]);
+console.log(accountId);
