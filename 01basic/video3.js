a671b5f2b3930b4b2490f71f4ae11d3b5066e68e
Ayan Saha <ayanwork2003@gmail.com>
2026-01-05 16:18:51 +0000

    this is detailed datatypes

---
+++ 01basic/video3.js
@@ -0,0 +1,36 @@
+"use strict" // thoughh the js engine computes every program in modern js version : but we can use this "use strict " to change all 
+// program into or code into modern javascript;
+
+//alert(3+3) --> we are using node.js to run this js program it will not work here and if we use console we can get this as popup
+
+console.log(3+
+        3  // always write a clean and redable code for better understnding 
+)
+
+
+console.log("ayan");
+
+let name= " Ayan";
+let age= 18;
+
+let isloggedin=false;
+let state;
+
+/*
+    number range in js is --> 2^53 near about ;
+
+    if the nnumber is out of this range we can use "big int";
+
+    string = " " --> alawys denote string  under this double coutes;
+
+    boolean= true// false it can be either true or false ;
+
+    null --> is a standalone value if we ask type it is an object;
+
+    undefined --> we can say that any var assinged in memory and its value is initially not present or undefined in simple words
+
+    symbol--> unique;
+*/
+
+console.log(typeof(null)); // object
+console.log(typeof(age)); // number  // we can see the usage of typeof() it can easily tell us the type of let or const and console.log can show that
