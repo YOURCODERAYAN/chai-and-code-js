f3351722743d498884e33e4d991ea416cacbc0b3
Ayan Saha <ayanwork2003@gmail.com>
2026-01-12 16:32:57 +0000

    new concepts

---
+++ 03basic/function.js
@@ -0,0 +1,59 @@
+/*function addTwoNum(number1 , number2){
+        console.log(number1+number2);
+}
+addTwoNum(3,4);
+
+    function addnew(a , b){
+
+            sum= a+b;
+        return sum;                 // NOW HERE WE ARE TAKING ONLY NUMBER OR TYPE INTEGER TO DO THE OPERATION
+    }
+
+
+console.log(addnew(4,5));
+
+
+function newadd( x , y){
+        if(Number.isInteger(x) && Number.isInteger(y)){
+                sum= x+y;
+                return sum;
+        }                                               // simple metjod of function but just using if else statement to check // 
+        else{
+                return 0;
+        }
+}
+
+console.log(newadd(5,6));*/
+
+
+//function calculateCartPrice( ...num1){
+   // return num1 ;
+ //}
+
+   // console.log(calculateCartPrice(100 , 200 , 300 , 400));
+   
+//const user = {
+  //  username: "hitesh",      // declaration of objects // 
+  //  prices: 199
+ //}
+
+//function handleObject(anyobject){
+ //   console.log(`the username is ${anyobject.username} and the prices are ${anyobject.prices}`); // now we are decalring the function and declaring the object//
+ //} 
+
+  //  console.log(handleObject(user));
+
+//handleObject({
+   // username: "Ayan",
+   // prices: 2000
+ //}) 
+    
+
+const myNewArray = [200, 400, 100, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+
+console.log(returnSecondValue(myNewArray));
+
